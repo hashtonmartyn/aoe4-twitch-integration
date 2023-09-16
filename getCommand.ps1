ef1471d29dfd6b3d20d30ec9f68bf254f36b81f3
Henry Ashton-Martyn <henry.ashton.martyn@gmail.com>
2023-09-16 23:31:41 +1200

    make the powershell script accept the twitch username as a cli arg

--- getCommand.ps1
+++ getCommand.ps1
@@ -1,7 +1,20 @@
+"return ''" | Out-File -FilePath "C:\aoe4ti\poll_result.scar" -Encoding UTF8
+
 while ($true) {
-    echo "Get"
-    $response = Invoke-WebRequest -Uri "http://localhost:8000/poll_result/HeneroAOE" -Method 'GET' -Headers @{'Accept' = 'application/json'}
+    $twitchUser = $args[0]
+    $uri = "https://backend.aoe4ti.com/poll_result/$twitchUser"
+    Write-Host "Get $uri"
+    try {
+        $response = Invoke-WebRequest -Uri $uri -Method 'GET' -Headers @{'Accept' = 'application/json'}
+    } catch {
+        Write-Host "Error: $_"
+        Start-Sleep -Seconds 2
+        continue
+    }
+
     $rJson = $response.Content | ConvertFrom-Json
+    Write-Host "Response: $rJson"
+
     $result = $rJson.result
     $eventId = $rJson.event_id
     "return '$eventId,$result'" | Out-File -FilePath "C:\aoe4ti\poll_result.scar" -Encoding UTF8
